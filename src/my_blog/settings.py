8412acddcd2aa5894ce385d1a0dd6865b497b22c
Rida Essaih <ridaessaih@gmail.com>
2020-09-10 19:54:07 +0200

    make files: templates & static

--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogApp.apps.BlogappConfig',
+    'userApp.apps.UserappConfig',
 ]
 
 MIDDLEWARE = [
